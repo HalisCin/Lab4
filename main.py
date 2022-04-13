2c3fb482571911262aa2477e67bfdc3f212eea48
Halis <101893446+HalisCin@users.noreply.github.com>
2022-04-13 13:01:21 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,128 @@
+import random
+
+#01
+
+my_empty_list=[]
+
+print('My List :', my_empty_list)
+
+my_empty_list.append("Halis")
+print(my_empty_list)
+
+my_empty_list.append("Python")
+print(my_empty_list)
+
+my_empty_list.append("Guido van Rossum")
+print(my_empty_list)
+
+del my_empty_list[0]
+print(my_empty_list)
+
+my_empty_list.append("is the inventor of ")
+print(my_empty_list)
+
+my_empty_list.pop(0)
+print(my_empty_list)
+
+my_empty_list.append("Python")
+print(my_empty_list)
+
+
+#02
+
+
+#Try your “words” variable by printing a slice from the list. You may choose the begining and ending indexes of the slice. (1)
+
+
+words = ["a","ability","able","about","above","accept","according","account","across","act","action","activity","actually","add","address","administration","admit","adult","affect","after","again","against","age","agency","agent","ago","agree","agreement","ahead","air","all","allow","almost","alone","along","already","also","although","always","American","among","amount","analysis","and","animal","another","answer","any","anyone","anything","appear","apply","approach","area","argue","arm","around","arrive","art","article","artist","as","ask","assume","at","attack","attention","attorney","audience","author","authority","available","avoid","away","baby","back","bad","bag","ball","bank","bar","base","be","beat","beautiful","because","become","bed","before","begin","behavior","behind","believe","benefit","best","better","between","beyond","big","bill","billion","bit","black","blood","blue","board","body","book","born","both","box","boy","break","bring","brother","budget","build","building","business","but","buy","by","call","camera","campaign","can","cancer","candidate","capital","car","card","care","career","carry","case","catch","cause","cell","center","central","century","certain","certainly","chair","challenge","chance","change","character","charge","check","child","choice","choose","church","citizen","city","civil","claim","class","clear","clearly","close","coach","cold","collection","college","color","come","commercial","common","community","company","compare","computer","concern","condition","conference","Congress","consider","consumer","contain","continue","control","cost","could","country","couple","course","court","cover","create","crime","cultural","culture","cup","current","customer","cut","dark","data","daughter","day","dead","deal","death","debate","decade","decide","decision","deep","defense","degree","Democrat","democratic","describe","design","despite","detail","determine","develop","development","die","difference","different","difficult","dinner","direction","director","discover","discuss","discussion","disease","do","doctor","dog","door","down","draw","dream","drive","drop","drug","during","each","early","east","easy","eat","economic","economy","edge","education","effect","effort","eight","either","election","else","employee","end","energy","enjoy","enough","enter","entire","environment","environmental","especially","establish","even","evening","event","ever","every","everybody","everyone","everything","evidence","exactly","example","executive","exist","expect","experience","expert","explain","eye","face","fact","factor","fail","fall","family","far","fast","father","fear","federal","feel","feeling","few","field","fight","figure","fill","film","final","finally","financial","find","fine","finger","finish","fire","firm","first","fish","five","floor","fly","focus","follow","food","foot","for","force","foreign","forget","form","former","forward","four","free","friend","from","front","full","fund","future","game","garden","gas","general","generation","get","girl","give","glass","go","goal","good","government","great","green","ground","group","grow","growth","guess","gun","guy","hair","half","hand","hang","happen","happy","hard","have","he","head","health","hear","heart","heat","heavy","help","her","here","herself","high","him","himself","his","history","hit","hold","home","hope","hospital","hot","hotel","hour","house","how","however","huge","human","hundred","husband","I","idea","identify","if","image","imagine","impact","important","improve","in","include","including","increase","indeed","indicate","individual","industry","information","inside","instead","institution","interest","interesting","international","interview","into","investment","involve","issue","it","item","its","itself","job","join","just","keep","key","kid","kill","kind","kitchen","know","knowledge","land","language","large","last","late","later","laugh","law","lawyer","lay","lead","leader","learn","least","leave","left","leg","legal","less","let","letter","level","lie","life","light","like","likely","line","list","listen","little","live","local","long","look","lose","loss","lot","love","low","machine","magazine","main","maintain","major","majority","make","man","manage","management","manager","many","market","marriage","material","matter","may","maybe","me","mean","measure","media","medical","meet","meeting","member","memory","mention","message","method","middle","might","military","million","mind","minute","miss","mission","model","modern","moment","money","month","more","morning","most","mother","mouth","move","movement","movie","Mr","Mrs","much","music","must","my","myself","name","nation","national","natural","nature","near","nearly","necessary","need","network","never","new","news","newspaper","next","nice","night","no","none","nor","north","not","note","nothing","notice","now","n't","number","occur","of","off","offer","office","officer","official","often","oh","oil","ok","old","on","once","one","only","onto","open","operation","opportunity","option","or","order","organization","other","others","our","out","outside","over","own","owner","page","pain","painting","paper","parent","part","participant","particular","particularly","partner","party","pass","past","patient","pattern","pay","peace","people","per","perform","performance","perhaps","period","person","personal","phone","physical","pick","picture","piece","place","plan","plant","play","player","PM","point","police","policy","political","politics","poor","popular","population","position","positive","possible","power","practice","prepare","present","president","pressure","pretty","prevent","price","private","probably","problem","process","produce","product","production","professional","professor","program","project","property","protect","prove","provide","public","pull","purpose","push","put","quality","question","quickly","quite","race","radio","raise","range","rate","rather","reach","read","ready","real","reality","realize","really","reason","receive","recent","recently","recognize","record","red","reduce","reflect","region","relate","relationship","religious","remain","remember","remove","report","represent","Republican","require","research","resource","respond","response","responsibility","rest","result","return","reveal","rich","right","rise","risk","road","rock","role","room","rule","run","safe","same","save","say","scene","school","science","scientist","score","sea","season","seat","second","section","security","see","seek","seem","sell","send","senior","sense","series","serious","serve","service","set","seven","several","sex","sexual","shake","share","she","shoot","short","shot","should","shoulder","show","side","sign","significant","similar","simple","simply","since","sing","single","sister","sit","site","situation","six","size","skill","skin","small","smile","so","social","society","soldier","some","somebody","someone","something","sometimes","son","song","soon","sort","sound","source","south","southern","space","speak","special","specific","speech","spend","sport","spring","staff","stage","stand","standard","star","start","state","statement","station","stay","step","still","stock","stop","store","story","strategy","street","strong","structure","student","study","stuff","style","subject","success","successful","such","suddenly","suffer","suggest","summer","support","sure","surface","system","table","take","talk","task","tax","teach","teacher","team","technology","television","tell","ten","tend","term","test","than","thank","that","the","their","them","themselves","then","theory","there","these","they","thing","think","third","this","those","though","thought","thousand","threat","three","through","throughout","throw","thus","time","to","today","together","tonight","too","top","total","tough","toward","town","trade","traditional","training","travel","treat","treatment","tree","trial","trip","trouble","true","truth","try","turn","TV","two","type","under","understand","unit","until","up","upon","us","use","usually","value","various","very","victim","view","violence","visit","voice","vote","wait","walk","wall","want","war","watch","water","way","we","weapon","wear","week","weight","well","west","western","what","whatever","when","where","whether","which","while","white","who","whole","whom","whose","why","wide","wife","will","win","wind","window","wish","with","within","without","woman","wonder","word","work","worker","world","worry","would","write","writer","wrong","yard","yeah","year","yes","yet","you","young","your","yourself"]
+
+print(words[4:10])
+
+
+#Write a Python Script  (2)
+
+
+print("\nEnter value between 5-10")
+user_number = int(input('Enter the length of password: '))
+while True:
+    if user_number < 5 or user_number > 10:
+       print("wrong input")
+    else:
+        sample_list = random.sample(words, user_number)
+        print(sample_list)
+        s = "".join(sample_list)
+        print(s)
+        break
+
+
+
+#03
+
+
+#1 to 30 Square  (1)
+
+number = int(input("Please enter the Maximum Number : "))
+myDict = {x: x ** 2 for x in range(1, number + 1)}
+print("\nDictionary = ", myDict)
+
+#Each key-value pair in a different line  (2)
+
+number = int(input("Please enter the Maximum Number : "))
+myDict = {x: x ** 2 for x in range(1, number + 1)}
+
+for key, value in myDict.items():
+    print(key, ' : ', value)
+
+
+#compute and print the sum of all the values in the dictionary without using sum() function. (3)
+
+
+def returnSum(myDict):
+    list = []
+    for i in myDict:
+        list.append(myDict[i])
+    final = sum(list)
+
+    return final
+
+
+
+#04
+
+#Merge Dictionary (1)
+
+d1 = {'Tony': 41, 'Steve': 39, 'Nat': 35}
+d2 = {'Bruce': 41, 'Clint': 35, 'Thor': 38}
+
+d=d1.copy()
+d.update(d2)
+print("Merged dictionary : " , d)
+
+#Remove NAT  (2)
+
+my_dict = {1: "Tony ", 2: "Steve",3: "Nat",4: "Bruce", 5:"Clint",6:"Thor"}
+popped_value = my_dict.pop(3)
+
+line = "The value removed from the dictionary of the key is : {}"
+print(line.format(popped_value))
+
+#Print Merged Dictionary   (3)
+
+d.keys()
+sorted(d.keys())
+
+for key in sorted(d.keys()):
+    print(key, "::", d[key])
+
+#Max Min Value   (4)
+
+d = {'Tony':41, 'Steve':39, 'Nat': 35,'Bruce': 41 ,'Clint':35 ,'Thor':38}
+
+key_max = max(d.keys(), key=(lambda k: d[k]))
+key_min = min(d.keys(), key=(lambda k: d[k]))
+
+print('Maximum Value: ',d[key_max])
+print('Minimum Value: ',d[key_min])
+
+
+
